fb220aa580d6412472664f79d0fc0c8a1da890c9
Alissia Sicuro <sicuroa@bzz.ch>
2025-10-28 10:32:13 +0100

    exercise2 fertig

--- src/main/java/exercise2/Main.java
+++ src/main/java/exercise2/Main.java
@@ -3,6 +3,13 @@ package exercise2;
 public class Main {
     public static void main(String[] args) {
         //Main Method
+        Patient meinPatient = new Patient("1", "Bruno",24);
+        Patient sachPatient = new Patient("2", "Ameli",17);
+
+        meinPatient.getAge();
+        meinPatient.setAge(44);
+        System.out.println(meinPatient);
+        System.out.println(sachPatient);
 
     }
 }
--- src/main/java/exercise2/Patient.java
+++ src/main/java/exercise2/Patient.java
@@ -3,15 +3,49 @@ package exercise2;
 public class Patient {
 
     //TODO: Declare Private Attributes
+    private String id;
+    private String name;
+    private int age;
+
 
     //TODO: Create Constructor
+    public Patient(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 
     //TODO: Add Getter and Setter Methods
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     //TODO: Add toString Method
     public String toString() {
 
-        return "";
+        return "Mein Name ist " + name + " ich bin " + age + " jahre alt " +" und meine id ist " + id;
     }
 
 }
