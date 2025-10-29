8ab8f148b6eda3d49781d8c39ff2ab84019337a2
Alissia Sicuro <sicuroa@bzz.ch>
2025-10-29 08:31:33 +0100

    exercise3 Fertig

--- src/main/java/exercise3/Car.java
+++ src/main/java/exercise3/Car.java
@@ -1,58 +1,64 @@
 package exercise3;
 
 public class Car {
-  public double fuelLevel;
-  public double mileage;
-  public String licensePlate;
-  public double fuelConsumption;
-  public double fuelCapacity;
+    public double fuelLevel;
+    public double mileage;
+    public String licensePlate;
+    public double fuelConsumption;
+    public double fuelCapacity;
 
 
-  //declare vars
+    //declare vars
 
-  //Create constructor
+    //Create constructor
     public Car(String licensePlate, double fuelLevel, double mileage, double fuelConsumption, double fuelCapacity) {
-      this.licensePlate = licensePlate;
-      this.fuelConsumption = fuelConsumption;
-      this.fuelCapacity = fuelCapacity;
-      this.mileage = mileage;
-      this.fuelLevel = fuelLevel;
+        this.licensePlate = licensePlate;
+        this.fuelConsumption = fuelConsumption;
+        this.fuelCapacity = fuelCapacity;
+        this.mileage = mileage;
+        this.fuelLevel = fuelLevel;
 
     }
 
-        public void drive(int distance) {
-            //do stuff
-            double maxdistance = fuelLevel / fuelConsumption;
-            System.out.println("Max distance: " + maxdistance);
-            double distanceToDrive = distance;
-            System.out.println(fuelLevel);
-            if(distance > maxdistance){
-                distanceToDrive =maxdistance;
-            }
-            this.fuelLevel -= distanceToDrive * this.fuelConsumption;
-            this.mileage = Math.round ((this.mileage + distanceToDrive) * 100.0) / 100.0;
-            if (this.fuelLevel < 0) {
-                this.fuelLevel = 0;
-
-            }
+    public void drive(int distance) {
+        //do stuff
+        double maxdistance = fuelLevel / fuelConsumption;
+        System.out.println("Max distance: " + maxdistance);
+        double distanceToDrive = distance;
+        System.out.println(fuelLevel);
+        if (distance > maxdistance) {
+            distanceToDrive = maxdistance;
         }
+        this.fuelLevel -= distanceToDrive * this.fuelConsumption;
+        this.mileage = Math.round((this.mileage + distanceToDrive) * 100.0) / 100.0;
+        if (this.fuelLevel < 0) {
+            this.fuelLevel = 0;
 
-        public void refuel(int amount) {
-            //do
-            this.fuelLevel = fuelLevel + amount;
-
-        }
-
-        //add toString
-        public String toString(){
-            return "Car{" +
-                    "licensePlate='" + licensePlate + '\'' +
-                    ", mileage=" + mileage +
-                    ", fuelConsumption=" + fuelConsumption +
-                    ", fuelCapacity=" + fuelCapacity +
-                    ", fuelLevel=" + fuelLevel +
-                    '}';
         }
     }
+
+    public void refuel(int amount) {
+        //do stuff
+
+        if (this.fuelLevel + amount > fuelCapacity) {
+            this.fuelLevel = fuelCapacity;
+        } else {
+            this.fuelLevel = this.fuelLevel + amount;
+        }
+
+
+    }
+
+    //add toString
+    public String toString() {
+        return "Car{" +
+                "licensePlate='" + licensePlate + '\'' +
+                ", mileage=" + mileage +
+                ", fuelConsumption=" + fuelConsumption +
+                ", fuelCapacity=" + fuelCapacity +
+                ", fuelLevel=" + fuelLevel +
+                '}';
+    }
+}
 
 
--- src/main/java/exercise3/Main.java
+++ src/main/java/exercise3/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //TODO: Create Car Object
 Car meinCar = new Car("ZH420", 62000, 60, 0.077, 50);
-Car deinCar = new Car("ZH422", 62000, 60, 0.077, 5);
+Car deinCar = new Car("ZH422", 62000, 60, 0.077, 60);
 
+        System.out.println(meinCar);
+        System.out.println(deinCar);
     }
 }
